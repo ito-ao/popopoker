e87e3b027a5b0771fef8f6f6f0b0ef3fa4b2adf6
Ao-ito0 <Ao-ito0@AOPC05>
2019-09-04 18:36:15 +0900

    ポーカーつくりはじめました

---
+++ Tranpu/src/Card.java
@@ -0,0 +1,11 @@
+
+public class Card {
+
+	private Suit suit;	//カードの柄
+	private int num;	//数字
+	private String str;//
+	public static  enum Suit{DAIYA,CLBU,SPADE,HERAT}
+	private static  String[] tostr =  {"ダイヤ","クラブ","スペード","ハート"};
+	
+	
+}
