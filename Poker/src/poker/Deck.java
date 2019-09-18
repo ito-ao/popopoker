d56decec576145af3fa220e19d190f913ae330e7
Ao-ito0 <Ao-ito0@AOPC05>
2019-09-18 16:51:45 +0900

    poker

--- Poker/src/poker/Deck.java
+++ Poker/src/poker/Deck.java
@@ -1,8 +1,9 @@
-package Poker;
+package poker;
 import java.util.Collections;
 import java.util.LinkedList;
 
-import Poker.Card.Suit;
+import poker.Card.Suit;
+
 
 
 public class Deck {
@@ -21,7 +22,7 @@ public class Deck {
 
 		//なぜリンクでやるか 手札と山札のことを考えるとAraayよりか効率的と考えたため。
 		//手札に加わるということは山札からそのカードは消えるので最低十枚は消えることになる
-		//なおかつ手札を捨てるとなると合計最大二十枚消えることになるためリンクのほうが効率的であると思いました。
+		//なおかつ手札を捨てるとなると合計最大二十枚消えることになるためリンクのほうが効率的である(たぶん)。
 		cards =	new LinkedList<Card>();
 		Suit[] suit = Suit.values();
 		for(int i =0; i<SUIT_NUM;i++) {
@@ -40,7 +41,49 @@ public class Deck {
 
 	public void printDeck() {
 		for (Card c:cards) {
+			System.out.printf("(%s, %d)\n", c.getSuit().name(), c.getNum());
 
 		}
 	}
+
+	public int getRestCardsNum() {
+		return cards.size();
+
+	}
+	/**
+	 *@param n
+	 *@throws DrawException
+	 * */
+
+	public Card[]draw(int n)throws DrawException{
+
+		if(getRestCardsNum()<n)throw new DrawException("山札にカードない");
+
+		Card[] res = new Card[n];
+
+		for(int i = 0; i < n; i++){
+			res[i] = cards.poll();
+		}
+
+		return res;
+	}
+	/**
+	@throws DrawException
+	**/
+	public Card draw()throws DrawException{
+		return draw(1)[0];
+	}
+	/**
+	 * @return
+	 */
+	public int countJokers() {
+		int jSum=0;
+		for(Card c:cards) {
+			if(c.getSuit()==Suit.JOKER) {
+				jSum++;
+			}
+		}
+		return jSum;
+	}
 }
+
